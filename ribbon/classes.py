6892bd4ff44d3d44384a2972b40ac63720183a13
Paolo D'Onorio De Meo <paolo.donoriodemeo@paradigm.co>
2022-04-08 12:59:13 +0200

    First draft

--- ribbon/classes.py
+++ ribbon/classes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # Created By: Steven (steven@ribbon.finance)
 # Created Date: 04/04/2022
 # version ='0.1.0'
@@ -12,32 +12,111 @@
 # Imports
 # ---------------------------------------------------------------------------
 from dataclasses import dataclass
+from enum import Enum, EnumMeta
+
 
 # ---------------------------------------------------------------------------
 # Data Classes
 # ---------------------------------------------------------------------------
 @dataclass
 class Domain:
-  """Domain parameters for signatures"""
-  name: str
-  chainId: int
-  verifyingContract: str
-  version: int
-  salt: str = None
+    """Domain parameters for signatures"""
+
+    name: str
+    chainId: int
+    verifyingContract: str
+    version: int
+    salt: str = None
+
 
 @dataclass
 class Bid:
-  """Bid parameters"""
-  swapId: int
-  nonce: int
-  signerWallet: str
-  sellAmount: int
-  buyAmount: int
-  referrer: str
+    """Bid parameters"""
+
+    swapId: int
+    nonce: int
+    signerWallet: str
+    sellAmount: int
+    buyAmount: int
+    referrer: str
+
 
 @dataclass
 class SignedBid(Bid):
-  """Signed bid fields"""
-  v: int
-  r: str
-  s: str
+    """Signed bid fields"""
+
+    v: int
+    r: str
+    s: str
+
+
+class MembershipTestEnumMeta(EnumMeta):
+    def __contains__(cls, item):
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+class BaseEnum(Enum, metaclass=MembershipTestEnumMeta):
+    """With this you can do membership tests,
+    e.g. >>> element in YourEnumClass"""
+
+
+class Chains(BaseEnum):
+    """Maybe to be called EtherumChains?
+    Note: to test membership you can do
+    >>> if my_current_chain in Chains.__members__
+    """
+
+    PROD = "mainnet"
+    TESTNET = "kovan"
+
+
+class InfuraAPIVersions(Enum):
+    V3 = "v3"
+
+
+INFURA_RPC_URLS = {
+    Chains.PROD: f"https://kovan.infura.io/{InfuraAPIVersions.V3}/",
+    Chains.TESTNET: f"https://mainnet.infura.io/{InfuraAPIVersions.V3}/",
+}
+
+
+@dataclass
+class ContractConfig:
+    """Configuration needed to connect to a Contract"""
+
+    chain_name: Chains = Chains.TESTNET
+    version: int = 1
+    # salt: str = None
+
+    @property
+    def infura_rpc_url(self):
+        return INFURA_RPC_URLS[self.chain_name]
+
+
+@dataclass
+class Vaults:
+    """Vault representation"""
+
+    address: str
+    bidding_token: str
+
+
+class EthereumVaults(BaseEnum):
+    """All available Vaults enabled by Ribbon"""
+
+    THETA_ETH_CALL = "RibbonThetaVaultETHCall"
+    THETA_WBTC_CALL = "RibbonThetaVaultWBTCCall"
+
+
+if __name__ == "__main__":
+    from dataclasses import asdict
+
+    config = ContractConfig()
+    print(asdict(config))
+    print(config.infura_rpc_url)
+    print("kovan" in Chains)
+    print("wrong" in Chains)
