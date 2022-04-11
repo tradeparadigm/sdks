c556208c776fdd4ca14305fc88b218f9be27deda
Steven <steven@ribbon.finance>
2022-04-11 16:06:58 +0800

    Added ERC20 details

--- ribbon/erc20.py
+++ ribbon/erc20.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# Created By: Steven (steven@ribbon.finance)
+# Created By: Steven@Ribbon, Paolo@Paradigm
 # Created Date: 08/04/2022
 # version ='0.1.0'
 # ---------------------------------------------------------------------------
@@ -24,14 +24,14 @@ class ERC20Contract(ContractConnection):
     Object to create connection to an ERC20 contract
 
     Args:
-        chain (str): The chain the contract is deployed in
-        address (str): Contract address
-        abi (dict): Contract ABI location
+        config (ContractConfig): Configuration to setup the Contract
     """
     abi_location = "abis/ERC20.json"
 
     def __init__(self, config: ContractConfig):
         super().__init__(config)
+        self.name = self.contract.functions.name().call()
+        self.symbol = self.contract.functions.symbol().call()
         self.decimals = self.contract.functions.decimals().call()
 
     def get_allowance(self, owner: str, spender: str) -> int:
