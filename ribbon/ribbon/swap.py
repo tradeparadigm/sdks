d9c624a8a389666aa3156f888f849ac86619acce
Ken Chan <guanhao3797@gmail.com>
2022-07-18 20:16:46 +0800

    Add validate_authority (#30)
    Function to be used to validate delegated wallet

--- ribbon/ribbon/swap.py
+++ ribbon/ribbon/swap.py
@@ -123,6 +123,20 @@ class SwapContract(ContractConnection):
                 ],
             }
 
+    def validate_authority(self, wallet: Wallet, authority_address: str) -> bool:
+        """
+        Method to verify authority
+
+        Args:
+            wallet_address (str): Address of the wallet
+            authority_address (str): Address of the authority
+
+        Returns:
+            verified (bool): True if the authority is set for the wallet
+        """
+        authorized = self.contract.functions.authorized(wallet.public_key).call()
+        return authorized == authority_address
+
     def create_offer(self, offer: Offer, wallet: Wallet) -> str:
         """
         Method to create offer
