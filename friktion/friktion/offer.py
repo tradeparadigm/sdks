a94a3fca614067038277b581df37fadf6fb8f1ea
Cassios Marques <cassios.marques@paradigm.co>
2022-07-21 10:50:27 -0300

    [friktion] Adjust functions interface to ease integration
    Also add seller to Offer object and fix some linter issues

--- friktion/friktion/offer.py
+++ friktion/friktion/offer.py
@@ -1,42 +1,29 @@
-from typing import Union
+from dataclasses import dataclass
+from typing import Optional
 
 from solana.publickey import PublicKey
 
 from friktion.friktion_anchor.accounts.swap_order import SwapOrder
 
 
-class Offer(object):
+@dataclass
+class Offer:
     oToken: PublicKey
     biddingToken: PublicKey
     offerAmount: int
     minPrice: int
     minBidSize: int
-    swapOrderAddress: Union[PublicKey, None]
-
-    def __init__(
-        self,
-        oToken: PublicKey,
-        biddingToken: PublicKey,
-        offerAmount: int,
-        minPrice: int,
-        minBidSize: int,
-    ):
-        self.oToken = oToken
-        self.biddingToken = biddingToken
-        self.offerAmount = offerAmount
-        self.minPrice = minPrice
-        self.minBidSize = minBidSize
-
-        self.swapOrderAddress = None
+    seller: PublicKey
+    swapOrderAddress: Optional[PublicKey] = None
 
     @staticmethod
     def from_swap_order(swap_order: SwapOrder, address: PublicKey):
-        o = Offer(
-            swap_order.give_mint,
-            swap_order.receive_mint,
-            swap_order.give_size,
-            0,
-            swap_order.give_size,
+        return Offer(
+            oToken=swap_order.give_mint,
+            biddingToken=swap_order.receive_mint,
+            offerAmount=swap_order.give_size,
+            minPrice=0,
+            minBidSize=swap_order.give_size,
+            swapOrderAddress=address,
+            seller=swap_order.creator,
         )
-        o.swapOrderAddress = address
-        return o
