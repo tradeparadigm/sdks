ff29b8fd2df5ee5255be9666536ce90ab085d0e7
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-08-23 14:14:02 +0200

    Temporary introduced type error to check the test matrix

--- template/template/config.py
+++ template/template/config.py
@@ -42,7 +42,7 @@ class TemplateSDKConfig(SDKConfig):
         swap_contract = SwapContract(config)
 
         new_offer = Offer(
-            oToken=oToken,
+            oToken=1,
             biddingToken=bidding_token,
             minBidSize=min_bid_size,
             minPrice=min_price,
